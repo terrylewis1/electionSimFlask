5538f32ff663320a5e17e1eded9c58bb08208dc9
terry39uab <terry39@uab.edu>
2018-11-20 20:16:40 -0600

    Adding bootstrap and template inheritance

--- electionSimFlask/electionsim.py
+++ electionSimFlask/electionsim.py
@@ -1,6 +1,31 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
+posts = [ 
+    {
+        'author' : 'Terry Lewis',
+        'title' :   'Blog post',
+        'content' : 'First post content',
+        'date_posted' : 'November 20, 2018'
+    }, 
+    {
+        'author' : 'Jane Doe',
+        'title' :   'Blog post',
+        'content' : 'Second post content',
+        'date_posted' : 'November 17, 2018'
+    }
+
+]
+
 @app.route("/")
-def hello():
-    return "<h1>Home Page</h1>"
+@app.route("/home")
+def home():
+    return render_template('home.html', posts = posts)
+
+@app.route("/about")
+def about():
+    return render_template('about.html', title = 'About Page')
+
+
+if __name__ == '__main__':
+    app.run(debug = True)
