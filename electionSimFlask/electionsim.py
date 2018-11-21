e7fd2cf7a82f60647abc31fee6c02f95dc0a0024
terry39uab <terry39@uab.edu>
2018-11-21 03:49:00 -0600

    Added registration and log in templates and logic.

--- electionSimFlask/electionsim.py
+++ electionSimFlask/electionsim.py
@@ -1,5 +1,8 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, flash, redirect
+from forms import RegistrationForm, LoginForm
 app = Flask(__name__)
+
+app.config['SECRET_KEY'] = '162d99e024e23c1faf1264d14cae51ad'
 
 posts = [ 
     {
@@ -25,6 +28,21 @@ def home():
 @app.route("/about")
 def about():
     return render_template('about.html', title = 'About Page')
+
+@app.route("/register", methods = ['GET', 'POST'])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit() :
+        flash(f'Account created for {form.username.data}!', 'success')
+        return redirect(url_for('home'))
+    return render_template('register.html', title= 'Register', form = form)
+
+@app.route("/login", methods = ['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit() :
+        return redirect(url_for('home'))
+    return render_template('login.html', title= 'Register', form = form)  
 
 
 if __name__ == '__main__':
